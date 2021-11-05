353465c0af02d32ef44de738708ea684baee6058
pangnem <sprnd645@gmail.com>
2021-11-05 16:15:04 +0900

    feat: AWS S3 관련 설정 추가
    - AWS와 관련된 민감한 정보들은 aws.yml로 관리하고, git에 등록되지 않기 위해 .gitignore에 추가하였습니다.
    - 어플리케이션 프로퍼티 설정에서 aws.yml 파일도 추가했습니다. `@SpringBootTest` 어노테이션을 사용할 때도 이 설정을 추가해야 합니다.
    
    See Also:
    - https://velog.io/@hellozin/Spring-Boot-Test%EC%97%90%EC%84%9C-Yaml-%ED%94%84%EB%A1%9C%ED%8D%BC%ED%8B%B0-%EC%A0%81%EC%9A%A9%ED%95%98%EA%B8%B0
    - https://devlog-wjdrbs96.tistory.com/323

--- src/test/java/io/devshare/controllers/HealthCheckControllerTest.java
+++ src/test/java/io/devshare/controllers/HealthCheckControllerTest.java
@@ -1,10 +1,5 @@
 package io.devshare.controllers;
 
-import static org.hamcrest.Matchers.containsString;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,7 +7,15 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
-@SpringBootTest
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest(properties = "spring.config.location=" +
+        "classpath:/application.yml" +
+        ",classpath:/aws.yml"
+)
 @AutoConfigureMockMvc
 public class HealthCheckControllerTest {
 
@@ -23,7 +26,7 @@ public class HealthCheckControllerTest {
     @DisplayName("서버가 살아있는지 체크한다.")
     void health_check() throws Exception {
         mockMvc.perform(get("/health-check"))
-            .andExpect(status().isOk())
-            .andExpect(content().string(containsString("I am Alive.")));
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("I am Alive.")));
     }
 }
