122bb7635dd6e39286b873158b647bc434501405
pangnem <sprnd645@gmail.com>
2022-07-05 10:18:52 +0900

    application 설정 파일 업데이트
    access-key가 아닌 accessKey임

--- src/main/java/io/devshare/DevshareApplication.java
+++ src/main/java/io/devshare/DevshareApplication.java
@@ -1,18 +1,12 @@
 package io.devshare;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class DevshareApplication {
-
-    public static final String APPLICATION_LOCATIONS = "spring.config.location="
-            + "classpath:application.yml,"
-            + "classpath:aws.yml";
-
-    public static void main(String[] args) {
-        new SpringApplicationBuilder(DevshareApplication.class)
-                .properties(APPLICATION_LOCATIONS)
-                .run(args);
-    }
+	public static void main(String[] args) {
+		new SpringApplication(DevshareApplication.class)
+				.run(args);
+	}
 }
